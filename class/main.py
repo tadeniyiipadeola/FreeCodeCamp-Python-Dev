d222207838c2f14e58eab698343866c23106ce04
Temi <stadeniy@cougarnet.uh.edu>
2021-12-08 20:01:24 -0600

    Contains the applicaction of instance attributes and Class attributes. __dict__ tags are used to view the instance level attributes or the class level attributes.

--- class/main.py
+++ class/main.py
@@ -1,5 +1,10 @@
-class item:
-    def __init__(self, name: str, price: int, quantity: int):
+class Item:
+    pay_rate = 0.8  # pay rate after the discount is applied
+    def __init__(self, name: str, price: float, quantity: int):
+        # Running validation to the received arguments
+        assert price >= 0, f'price {price} is not greater than or equal to zero!'
+        assert quantity >= 0, f'Quantity {quantity} is not greater or equal to zero!'
+        # Assign to self object
         self.name = name
         self.price = price
         self.quantity = quantity
@@ -7,9 +12,23 @@ class item:
     def total(self):
         return self.price * self.quantity
 
+    def apply_discount(self):
+        self.price = self.price * Item.pay_rate
 
-item1 = item('jeff', 10, 3)
+
+item1 = Item('jeff', 10, 3)
 cost = item1.total()
 
 print(item1.name, " ", item1.price, ' ', item1.quantity)
 print(cost)
+# instance attributes vs Class attributes
+# since Item1 instance couldn't find the pay_rate attribute the the instance attributes
+# it moves to the class attribute to find if the pat_rate can be found and there it is.
+print(Item.pay_rate)
+
+print(Item.pay_rate)
+print(Item.pay_rate)
+print(Item.__dict__)  # All the attributes for class level
+print(item1.__dict__)  # All the attributes for instance level
+item1.apply_discount()
+print(item1.price)
