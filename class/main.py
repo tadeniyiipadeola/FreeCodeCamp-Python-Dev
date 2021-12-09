00e278c87d7290061229349f9bad128f7e713320
Temi <stadeniy@cougarnet.uh.edu>
2021-12-08 18:15:48 -0600

    Created class with constructor looks great!!

--- class/main.py
+++ class/main.py
@@ -1,14 +1,15 @@
 class item:
-    def total(self, x, y):
-        return x * y
+    def __init__(self, name: str, price: int, quantity: int):
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+    def total(self):
+        return self.price * self.quantity
 
 
-item1 = item()
-item1.name = "jeff"
-item1.price = 10
-item1.quantity = 3
-
-cost = item1.total(item1.price, item1.quantity)
+item1 = item('jeff', 10, 3)
+cost = item1.total()
 
 print(item1.name, " ", item1.price, ' ', item1.quantity)
 print(cost)
