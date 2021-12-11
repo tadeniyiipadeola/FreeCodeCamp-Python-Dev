fc56857ddfb868332b1b699cb123064c23b4a61a
Temi <stadeniy@cougarnet.uh.edu>
2021-12-11 16:40:30 -0600

    This section explains how to use private and public methods or setter and getter in python.

--- class/Item.py
+++ class/Item.py
@@ -10,11 +10,19 @@ class Item:
         assert price >= 0, f'price {price} is not greater than or equal to zero!'
         assert quantity >= 0, f'Quantity {quantity} is not greater or equal to zero!'
         # Assign to self object
-        self.name = name
-        self.price = price
+        self.__name = name #one underscore restricts, making the attribute read only from outside the class.
+        self.price = price #double underscore makes it private for read only but un acccessable outsidethe class. Setter required
         self.quantity = quantity
         # Append the instance to the all list
         Item.all.append(self)
+    #property has become restricted or private. to access you need a setter function
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, value):
+        self.__name= value
 
     def calc_total_price(self):
         return self.price * self.quantity
@@ -50,3 +58,8 @@ class Item:
     # Used to display the list of instances
     def __repr__(self):
         return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity})"
+
+    #Property Decorator = Read-only
+    @property  # decorator allow you to make attributes read only, cant change the attribute after it's been instantiated
+    def read_only_name(self):
+        return "AAA"
