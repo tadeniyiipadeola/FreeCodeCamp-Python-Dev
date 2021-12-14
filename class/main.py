bbe847144020fd2078385524531442fe0e25f721
Temi <stadeniy@cougarnet.uh.edu>
2021-12-13 20:14:07 -0600

    This section explains how to use private and public methods or setter and getter in python.

--- class/main.py
+++ class/main.py
@@ -2,7 +2,9 @@ import csv
 from Item import Item
 from Phone import Phone
 
-phone1 = Phone('jscPhonev10', 500, 5, 1)
-phone2 = Phone('jscPhonev10', 700, 5, 1)
-print(Item.all)
-print(Phone.all)
+item1 = Item('MyItem', 750)
+
+item1.price = -900
+
+print(item1)
+
